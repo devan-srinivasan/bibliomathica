a08d7982d1df4dbba7f1a89006cd006c04d4f8f6
Devan Srinivasan <dsrinivasan@deloitte.ca>
2022-07-19 11:02:57 -0400

    added explore page

--- src/bibliomath/urls.py
+++ src/bibliomath/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.explore, name='explore'),
     path('puzzle/', TemplateView.as_view(template_name='puzzle.html'), name='puzzle'),
     path('explore/', views.explore, name='explore'),
+    path('base/', TemplateView.as_view(template_name='base.html'), name='base'),    # to be deleted
 ]
--- src/bibliomath/views.py
+++ src/bibliomath/views.py
@@ -4,5 +4,14 @@ from django.shortcuts import render
 def explore(request):
     # make some request to MongoDB
     # get data and format it accordingly to display
-    context = {}
+    topics = [
+        {  
+            'title': 'Geometry', 
+            'description': 'Everything to do with shapes',
+            'link': 'https://youtube.com',
+        }
+    ] * 5
+    context = {
+        'topics': topics
+    }
     return render(request, 'bibliomath/explore.html', context)
