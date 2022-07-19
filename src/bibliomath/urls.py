d4eb03d03af01575b421737a34a071d59fa8c66a
Devan Srinivasan <dsrinivasan@deloitte.ca>
2022-07-19 11:37:45 -0400

    restructured css, added collection page

--- src/bibliomath/urls.py
+++ src/bibliomath/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.explore, name='explore'),
     path('puzzle/', TemplateView.as_view(template_name='puzzle.html'), name='puzzle'),
     path('explore/', views.explore, name='explore'),
+    path('collection/', views.collection, name='collection'),
     path('base/', TemplateView.as_view(template_name='base.html'), name='base'),    # to be deleted
 ]
--- src/bibliomath/views.py
+++ src/bibliomath/views.py
@@ -15,3 +15,20 @@ def explore(request):
         'topics': topics
     }
     return render(request, 'bibliomath/explore.html', context)
+
+def collection(request):
+    # make some request to MongoDB
+    # get data and format it accordingly to display
+    resources = [
+        {  
+            'title': 'Colliding Masses', 
+            'description': 'Why do the number of collisions between colliding masses, all powers of ten, spit out the digits of pi?',
+            'link': 'https://www.youtube.com/watch?v=jsYwFizhncE',
+            'complete': False,
+            'favourite': True,
+        }
+    ] * 3
+    context = {
+        'resources': resources,
+    }
+    return render(request, 'bibliomath/collection.html', context)
