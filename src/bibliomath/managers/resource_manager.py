abb1036501b4ed70db76bd4d566f2935ae0fd0bb
Devan Srinivasan <83255301+devan-srinivasan@users.noreply.github.com>
2022-07-20 11:05:03 -0400

    can't add duplicate resources, cleaner view code, added redirections when adding a resource or topic (it's rly clean) (#24)
    Co-authored-by: Devan Srinivasan <dsrinivasan@deloitte.ca>
    
    Resolves #22
    Resolves #21 
    
    Merged to master

---
+++ src/bibliomath/managers/resource_manager.py
@@ -0,0 +1,24 @@
+from ftplib import all_errors
+from bibliomath.models import Resource
+from bibliomath.helper import jsonify
+
+
+class ResourceManager:
+
+    def __init__(self) -> None:
+        pass
+        
+    def get_resources(self):
+        return jsonify(Resource.objects.all())
+
+    def get_resources_topic(self, topic):
+        return jsonify(Resource.objects.filter(topic=topic))
+    
+    def add_resource(self, resource):
+        all_res = self.get_resources()
+        for r in all_res:
+            if r['title'] == resource['title'] or r['link'] == resource['link']:
+                return False
+        new_res = Resource(title=resource['title'], description=resource['description'], link=resource['link'], topic=resource['topic'])
+        new_res.save()
+        return True
