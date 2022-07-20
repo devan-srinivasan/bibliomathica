d1c0ef647e0a8e15baee7df94a387461f5a5071f
Devan Srinivasan <83255301+devan-srinivasan@users.noreply.github.com>
2022-07-20 12:08:55 -0400

    using boostrap and adding some css (#25)
    Co-authored-by: Devan Srinivasan <dsrinivasan@deloitte.ca>
    
    Resolves #11

--- src/bibliomath/managers/resource_manager.py
+++ src/bibliomath/managers/resource_manager.py
@@ -19,6 +19,8 @@ class ResourceManager:
         for r in all_res:
             if r['title'] == resource['title'] or r['link'] == resource['link']:
                 return False
+        if not resource['link'].contains('https://'):
+            return False
         new_res = Resource(title=resource['title'], description=resource['description'], link=resource['link'], topic=resource['topic'])
         new_res.save()
         return True
