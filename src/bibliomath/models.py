c4c166d75031867793a25763124299539c3a879a
Devan Srinivasan <83255301+devan-srinivasan@users.noreply.github.com>
2022-07-19 21:42:40 -0400

    added fuctionality to create a resource (#18)
    * added fuctionality to create a resource
    
    * modified form to make get
    
    Resolves #16
    Resolves #13
    
    Co-authored-by: Devan Srinivasan <dsrinivasan@deloitte.ca>

--- src/bibliomath/models.py
+++ src/bibliomath/models.py
@@ -1,3 +1,12 @@
+from pydoc import describe
 from django.db import models
 
 # Create your models here.
+class Resource(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    topic = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
--- src/bibliomath/views.py
+++ src/bibliomath/views.py
@@ -1,18 +1,18 @@
+from pydoc_data.topics import topics
+import re
 from django.shortcuts import render
+from .models import Resource
+from bibliomath.topics import TopicsList
+
+# TOPICS
+TOPIC_LIST = TopicsList()
 
 # Create your views here.
 def explore(request):
     # make some request to MongoDB
     # get data and format it accordingly to display
-    topics = [
-        {  
-            'title': 'Geometry', 
-            'description': 'Everything to do with shapes',
-            'link': 'https://youtube.com',
-        }
-    ] * 5
     context = {
-        'topics': topics
+        'topics': TOPIC_LIST.get_topics(),
     }
     return render(request, 'bibliomath/explore.html', context)
 
@@ -32,3 +32,16 @@ def collection(request):
         'resources': resources,
     }
     return render(request, 'bibliomath/collection.html', context)
+
+def create(request):
+    res_topic = request.GET.get('topic', '')
+    res_title = request.GET.get('title', '')
+    res_description = request.GET.get('description', '')
+    res_link = request.GET.get('link', '')
+    new_resource = Resource(title=res_title, description=res_description, link=res_link, topic=res_topic)
+    new_resource.save()
+    context = {
+        'topics': TOPIC_LIST.get_topics(),
+    }
+    print(context)
+    return render(request, 'bibliomath/explore.html', context)
