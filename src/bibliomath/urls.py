daa10fdf3130632325daed73a6c9e438bf4ca698
Devan Srinivasan <dsrinivasan@deloitte.ca>
2022-07-19 21:05:48 -0400

    added fuctionality to create a resource

--- src/bibliomath/urls.py
+++ src/bibliomath/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('puzzle/', TemplateView.as_view(template_name='puzzle.html'), name='puzzle'),
     path('explore/', views.explore, name='explore'),
     path('collection/', views.collection, name='collection'),
-    path('base/', TemplateView.as_view(template_name='base.html'), name='base'),    # to be deleted
+    path('create/', views.create, name='create'),    # to be deleted
 ]
