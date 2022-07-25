3b36ac38c2a7be9f80af35ad058e67393b533f67
Devan Srinivasan <dsrinivasan@deloitte.ca>
2022-07-25 09:30:16 -0400

    fixing urls organization

--- src/bibliomath/urls.py
+++ src/bibliomath/urls.py
@@ -5,12 +5,7 @@ from .views import ResourceListView, ResourceDetailView, ResourceCreateView, Res
 
 urlpatterns = [
     path('', views.explore, name='explore'),
-<<<<<<< HEAD
-=======
-    path('puzzle/', TemplateView.as_view(template_name='puzzle.html'), name='puzzle'),
-    path('explore/', views.explore, name='explore'),
     #path('collection/', views.collection, name='collection'),
->>>>>>> e4a66e9e99f3c093d42a4b9b54c0c4fcbdb56c56
     path('sudo/', ResourceListView.as_view(), name='sudo'),
     path('sudo/<int:pk>', ResourceDetailView.as_view(), name='resource-detail'),
     path('sudo/new/', ResourceCreateView.as_view(), name='resource-create'),
